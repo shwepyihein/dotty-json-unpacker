ae0906ebfea498fe6df4e65b9fee65a9b8ce1efd
shwepyihein <shwepyihein.12@gmail.com>
2025-05-14 11:16:34 +0700

    build fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster as Sonner } from '@/components/ui/sonner';
 import { Toaster } from '@/components/ui/toaster';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Index from './pages/Index';
